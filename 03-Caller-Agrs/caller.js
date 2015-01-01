2c41017443e5121e89095c92370b6331cdcf08dd
Shining Yang <y.s.n@live.com>
2015-01-01 23:53:25 +0800

    First program in 2015, :)

---
+++ 03-Caller-Agrs/caller.js
@@ -0,0 +1,21 @@
+/**
+ * caller()
+ * Shining Yang <y.s.n@live.com>, 2014-01-01
+ */
+
+function myCaller() {
+	if (myCaller.caller) {
+		console.log('Caller: ',  myCaller.caller.toString());
+	} else {
+		console.log('Its a top function call.');
+	}
+}
+
+console.log('test case 1:');
+(function myCallerInvoker() {
+	myCaller();
+})();
+
+console.log('test case 2:');
+myCaller();
+
