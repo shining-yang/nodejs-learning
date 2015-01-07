666eafed2a0a2ffe2209c4eeb97c68e66265229a
shining-yang <shanning_yang@cn.dlink.com>
2015-01-07 09:52:00 +0800

    Stream query rows

--- 04-mysql/test-query.js
+++ 04-mysql/test-query.js
@@ -9,12 +9,18 @@ var conn = mysql.createConnection({host: 'localhost', user: 'root', password: '1
 conn.query('use mysql_test');
 conn.query('create table if not exists ' + table + ' (id int, name varchar(32))');
 
-var sql = conn.query('SELECT COUNT(*) from ??', table, function(err, row) {
-	if (err) {
-		console.log('Error occurred.' + err);
-	} else {
-		console.log(row);
-	}
+var sql = conn.query('SELECT * from ??', table);
+
+sql.on('error', function(err) {
+	console.log(err);
+}).on('fields', function(fields) {
+	console.log('Handling `fileds`: ');
+	console.log(fields);
+}).on('result', function(rows) {
+	console.log('Handling `rows`: ');
+	console.log(rows);
+}).on('end', function() {
+	console.log('All rows received.');
 });
 
 console.log(sql.sql);
