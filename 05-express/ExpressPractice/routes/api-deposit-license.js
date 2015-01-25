94df8f58c6ebf450645abcb1978518760e8566f4
shining-yang <venus@mint>
2015-01-25 23:36:13 +0800

    Introduce responseWithError()

--- 05-express/ExpressPractice/routes/api-deposit-license.js
+++ 05-express/ExpressPractice/routes/api-deposit-license.js
@@ -4,24 +4,50 @@
 var mysql = require('mysql');
 var util = require('util');
 
+// 420-02
+function responseWithError(res, pretty, errStatus, errCode) {
+  var resJson = {
+    errors: {
+    }
+  };
+  
+  resJson.errors.code = errCode;
+  switch (errCode) {
+    case '400-01':
+      resJson.errors.message = 'Syntax Error. The syntax is not correct or missing some parameters';
+      break;
+    case '420-02':
+      resJson.errors.message = 'Method Failure. The database is disconnected';
+      break;
+    case '406-01':
+      resJson.errors.message = 'Not Acceptable. The organization is not in exhausted mode';
+      break;
+    case '406-02':
+      resJson.errors.message = 'Not Acceptable. The organization is not existed';
+      break;
+    default:
+      resJson.errors.message = 'Unknown error';
+      break;
+  }
+    
+  if (pretty == 'true') {
+    res.status(errStatus).end(JSON.stringify(resJson, null, 3));
+  } else {
+    res.status(errStatus).end(JSON.stringify(resJson));
+  }
+}
+
 // check whether the format of posted data is valid
 function checkRequestFormat(requests) {
-  var tmpTest = '';
-  if (!tmpTest) {
-    console.log('tempTest is empty string. but considered as true');
-  }
-  
-  console.log(util.inspect(requests[0].license_id));
-  
   for (var i = 0; i < requests.length; i++) {
     if (!requests[i].license_id
-    //  || requests[i].constructor != String
+      || requests[i].license_id.constructor != String
       || requests[i].license_id.length <= 0) {
       return false;
     }
     
     if (!requests[i].deposited_by
-     // || requests[i].deposited_by.constructor != String
+      || requests[i].deposited_by.constructor != String
       || requests[i].deposited_by.length <= 0) {
       return false;
     }
@@ -58,27 +84,14 @@ function getDuplicateLicenseIds(requests) {
   return idDuplicate;
 }
 
-//
 // API: deposit licenses
-//
 function apiDepositLicense(req, res) {
   // check the API syntax
   if (!req.body
     || !Array.isArray(req.body.requests)
     || !checkRequestFormat(req.body.requests)) {
-    var resJson = {
-			errors: {
-        code: '400-01',
-        message: 'Syntax Error. The syntax is not correct or missing some parameters'
-      }
-    };
-    
-    if (req.query.pretty == 'true') {
-      res.status(400).end(JSON.stringify(resJson, null, 3));
-    } else {
-      res.status(400).end(JSON.stringify(resJson));
-    }
-    return;  
+    responseWithError(res, req.query.pretty, 400, '400-01');
+    return;
   }
   
   // check if there are some duplicated requests
@@ -116,41 +129,51 @@ function apiDepositLicense(req, res) {
   };
 
   console.log('Connecting mysql ...');
-
+   
   var sqlConn = mysql.createConnection(options);
   sqlConn.connect(function(err) {
     if (err) {
-      var resJson = {
-        errors: {
-          code: '420-02',
-          message: 'Method Failure. The database is disconnected'
-        }
-      };
-        
-      if (req.query.pretty == 'true') {
-        res.status(420).end(JSON.stringify(resJson, null, 3));
-      } else {
-        res.status(420).end(JSON.stringify(resJson));
-      }
+      responseWithError(res, req.query.pretty, 420, '420-02');
       return;
     }
     
-    var sql = 'SELECT state FROM organization WHERE id=??';
-    sqlConn.query(sql, [req.query.orgId], function(err, result) {
+    var sql = 'SELECT state FROM organization WHERE name=?';
+    sqlConn.query(sql, req.query.orgId, function(err, result) {
+      if (err) {
+        responseWithError(res, req.query.pretty, 420, '420-02');
+        sqlConn.end();
+        return;
+      }
       
+      if (result.length <= 0) { // no records
+        responseWithError(res, req.query.pretty, 406, '406-02');
+        sqlConn.end();
+        return;
+      }
+      
+      if (result[0].state === 'deducting') {
+        responseWithError(res, req.query.pretty, 406, '406-01');
+        sqlConn.end();
+        return;
+      }
+      
+      console.log(result);
+    
+      
+      console.log('Finish accessing mysql');
+    
+      var resSuccess = {};
+      resSuccess.licenses = req.body.requests;
+
+      if (req.query.pretty == 'true') {
+        res.status(200).end(JSON.stringify(resSuccess, null, 3));
+      } else {
+        res.status(200).end(JSON.stringify(resSuccess));
+      }
     });
-    
-    console.log('Finish accessing mysql');
+
     sqlConn.end();
-    
-    var resSuccess = {};
-    resSuccess.licenses = req.body.requests;
-    
-    if (req.query.pretty == 'true') {
-      res.status(200).end(JSON.stringify(resSuccess, null, 3));
-    } else {
-      res.status(200).end(JSON.stringify(resSuccess));
-    }
+    res.status(400).end('Error occurred');
   });
         
         /*
