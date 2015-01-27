ce7789fc332ff18203457d413d02c51fe16db0ac
shining-yang <shanning_yang@cn.dlink.com>
2015-01-27 15:41:25 +0800

    Check license validity

--- 05-express/ExpressPractice/routes/api-deposit-license.js
+++ 05-express/ExpressPractice/routes/api-deposit-license.js
@@ -61,12 +61,33 @@ function checkRequestFormat(requests) {
 // Determine if an array contains the specified element
 function arrayContains(arr, item) {
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == item) {
+    if (arr[i] === item) {
       return true;
     }
   }
 
   return false;
+}
+
+//
+function licenseContainsId(rows, id) {
+  for (var i = 0; i < rows.length; i++) {
+    if (rows[i].license_id === id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Get invalid licenses
+function filterInvalidLicenses(requests, rows) {
+  var idLicense = [];
+  for (var i = 0; i < requests.length; i++) {
+    if (!licenseContainsId(rows, requests[i].license_id)) {
+      idLicense.push(requests[i].license_id);
+    }
+  }
+  return idLicense;
 }
 
 // Get duplicated license ids
@@ -144,19 +165,20 @@ function apiDepositLicense(req, res) {
       res.status(420).end(buildErrorResponse('420-02', req.query.pretty));
     } else {
       var sql = 'SELECT state FROM organization WHERE name=?';
-      sqlConn.query(sql, [req.query.orgId], function (err, result) {
+      sqlConn.query(sql, [req.query.orgId], function (err, rows) {
+        console.log('Query statement: ' + sql);
         if (err) {
           res.status(420).end(buildErrorResponse('420-02', req.query.pretty));
           sqlConn.end();/*
-        } else if (result.length <= 0) {
-          res.status(406).end(buildErrorResponse('406-02', req.query.pretty));
-          sqlConn.end();
-        } else if (result[0].state == 'deducting') {
-          res.status(406).end(buildErrorResponse('406-01', req.query.pretty));
-          sqlConn.end();
-        } else if (result[0].state != 'normal' && result[0].state != 'exhausted') {
-          res.status(406).end(buildErrorResponse('406-13', req.query.pretty));
-          sqlConn.end();*/
+           } else if (rows.length <= 0) {
+           res.status(406).end(buildErrorResponse('406-02', req.query.pretty));
+           sqlConn.end();
+           } else if (rows[0].state == 'deducting') {
+           res.status(406).end(buildErrorResponse('406-01', req.query.pretty));
+           sqlConn.end();
+           } else if (rows[0].state != 'normal' && rows[0].state != 'exhausted') {
+           res.status(406).end(buildErrorResponse('406-13', req.query.pretty));
+           sqlConn.end();*/
         } else {
           // check usability
           var sql = 'SELECT * from license_generator where license_id in (';
@@ -167,16 +189,38 @@ function apiDepositLicense(req, res) {
             sql += '\'';
             sql += req.body.requests[i].license_id;
             sql += '\'';
-          }  
+          }
           sql += ')';
 
           console.log('SQL: ' + sql);
-          sqlConn.query(sql, function(err, result) {
+          sqlConn.query(sql, function(err, rows) {
             if (err) {
               res.status(420).end(buildErrorResponse('420-02', req.query.pretty));
               sqlConn.end();
+            } else if (rows.length !== req.body.requests.length) { // there are some licenses which cannot be selected out
+              var invalidLicenses = filterInvalidLicenses(req.body.requests, rows);
+              console.log('Dump all invalid license id: ');
+              console.log(invalidLicenses);
+
+              var resJson = {
+                errors: []
+              };
+
+              for (var i = 0; i < invalidLicenses.length; i++) {
+                resJson.errors.push({
+                  license_id: invalidLicenses[i],
+                  code: '406-05',
+                  message: 'Not Acceptable. The license is not existed'
+                });
+              }
+
+              if (req.query.pretty == 'true') {
+                res.status(406).end(JSON.stringify(resJson, null, 3));
+              } else {
+                res.status(406).end(JSON.stringify(resJson));
+              }
             } else {
-              console.log(result);
+              console.log(rows);
               res.status(200).end('{"error":"ok"}');
               sqlConn.end();
             }
