437458f3ed62316dfcdc34049be065b33d53915c
Shining Yang <y.s.n@live.com>
2015-12-29 13:43:43 +0800

    a bit more test

--- others/qcrl_wechat/api.js
+++ others/qcrl_wechat/api.js
@@ -3,35 +3,16 @@
 //
 var http = require('http');
 
-
 var hostname = 'stage.itis6am.com';
 //var hostname = '120.24.168.89';
 var pathname = '/apiWXTeacher/v1/wxMain';
 
-var postData = {
-  account: 'testwl',
-  password: 'e10adc3949ba59abbe56e057f20f883e',
-  openId: 'my-random-one',
-  client: '1',
-  method: 'login'
-};
-
-var options = {
-  hostname: hostname,
-  port: 80,
-  path: pathname,
-  method: 'POST',
-  headers: {
-    'Content-Type': 'applicaton/json',
-    'Content-Length': Buffer.byteLength(JSON.stringify(postData))
-  }
-};
 
 function performLogin(callback) {
   var postData = {
     account: 'testwl',
     password: 'e10adc3949ba59abbe56e057f20f883e',
-    openId: 'my-random-one',
+    openId: '我的OPEN-ID',
     client: '1',
     method: 'login'
   };
@@ -41,28 +22,33 @@ function performLogin(callback) {
     port: 80,
     path: pathname,
     method: 'POST',
-    headers: {
-      'Content-Type': 'applicaton/json',
-      'Content-Length': Buffer.byteLength(JSON.stringify(postData))
-    }
+    //headers: {
+    //  'Content-Type': 'applicaton/json',
+    //  'Content-Length': Buffer.byteLength(JSON.stringify(postData))
+    //}
   };
 
   var req = http.request(options, function (res) {
     console.log('HTTP STATUS: ', res.statusCode);
     res.setEncoding('utf-8');
 
-    res.on('data', function (data) {
-      var json = JSON.parse(data);
+    var reply;
+    res.on('data', function (chunk) {
+      if (reply) {
+        reply = Buffer.concat([reply, chunk]);
+      } else {
+        reply = chunk;
+      }
+    });
+
+    res.on('end', function () {
+      var json = JSON.parse(reply);
       if (json.code == 1) {
         console.log(JSON.stringify(json, null, 2));
         callback(json.data.openId);
       } else {
         console.log('Failed to login.');
       }
-    });
-
-    res.on('end', function () {
-      console.log('Finished login.');
     });
 
     res.on('error', function (err) {
@@ -87,24 +73,33 @@ function getOrderAmountInfo(openId) {
     port: 80,
     path: pathname,
     method: 'POST',
-    headers: {
-      'Content-Type': 'applicaton/json',
-      'Content-Length': Buffer.byteLength(JSON.stringify(postData))
-    }
+    //headers: {
+    //  'Content-Type': 'applicaton/json',
+    //  'Content-Length': Buffer.byteLength(JSON.stringify(postData))
+    //}
   };
 
   var req = http.request(options, function (res) {
     console.log('HTTP STATUS: ', res.statusCode);
     res.setEncoding('utf-8');
 
-    res.setEncoding('utf-8');
-    res.on('data', function (data) {
-      var json = JSON.parse(data);
-      console.log(JSON.stringify(json, null, 2));
+    var reply;
+    res.on('data', function (chunk) {
+      if (reply) {
+        reply = Buffer.concat([reply, chunk]);
+      } else {
+        reply = chunk;
+      }
     });
 
     res.on('end', function () {
-      console.log('Finished getting info.');
+      var json = JSON.parse(reply);
+      if (json.code == 1) {
+        console.log(JSON.stringify(json, null, 2));
+        callback(json.data.openId);
+      } else {
+        console.log('Failed to request.');
+      }
     });
 
     res.on('error', function (err) {
